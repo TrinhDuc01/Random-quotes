268f2e0685ce9f2b2bb3bf55f9d4ef78f1c06928
TrinhDuc01 <ductrinh.0301@gmail.com>
2025-06-11 16:31:18 +0700

    fix width

--- src/components/Container.js
+++ src/components/Container.js
@@ -6,7 +6,7 @@ const Container = () => {
     const { author } = useSelector(state => state.quotesReducer)
     const color = useSelector(state => state.colorReducer.color)
 
-    const style = { backgroundColor: "white", width: 500, height: 'auto', borderRadius: 10, padding: 30, boxShadow: "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px" }
+    const style = { backgroundColor: "white", maxWidth: "500px", height: 'auto', borderRadius: 10, padding: 30, boxShadow: "rgba(0, 0, 0, 0.25) 0px 14px 28px, rgba(0, 0, 0, 0.22) 0px 10px 10px" }
     return <div style={style}>
         <Text />
         <div style={{ display: "flex", flexDirection: "column", alignItems: 'flex-end', }}>
